965f380aca376b9979835f240a8f9c040a5acfa6
Tony Wu <tony.wu90@yahoo.com>
2018-09-21 21:15:15 -0500

    apply zuul service

---
+++ affablebean-proxy/src/main/java/tvo/tutorial/microservice/proxy/ApplicationBoot.java
@@ -0,0 +1,14 @@
+package tvo.tutorial.microservice.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApplicationBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBoot.class, args);
+	}
+}
