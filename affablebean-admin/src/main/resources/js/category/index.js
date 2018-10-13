04eeb922352b041c668ea54ca7c8d46abebe0201
Tony Wu <tony.wu90@yahoo.com>
2018-10-12 20:46:34 -0500

    affablebean-admin: reset, create, save

--- affablebean-admin/src/main/resources/js/category/index.js
+++ affablebean-admin/src/main/resources/js/category/index.js
@@ -8,7 +8,21 @@ class CategoryView extends Component {
         super(props);
         this.state = {data: this.props.data};
         this.renderEditable = this.renderEditable.bind(this);
-        this.renderColumns = this.renderColumns.bind(this);
+        this._cols = [{
+            accessor: 'id',
+            Header: 'ID'
+        }, {
+            accessor: 'name',
+            Header: 'Category Name',
+            Cell: this.renderEditable
+        }, {
+            accessor: 'imgUrl',
+            Header: 'Image URL',
+            Cell: this.renderEditable
+        }];
+        this.handleReset = this.handleReset.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }
 
     renderEditable(cellInfo) {
@@ -28,19 +42,19 @@ class CategoryView extends Component {
         );
     }
 
-    renderColumns() {
-        return [{
-            accessor: 'id',
-            Header: 'ID'
-        }, {
-            accessor: 'name',
-            Header: 'Category Name',
-            Cell: this.renderEditable
-        }, {
-            accessor: 'imgUrl',
-            Header: 'Image URL',
-            Cell: this.renderEditable
-        }];
+    handleReset() {
+        $.get(api.CATEGORY_API_FETCH, (data) => this.setState({data}));
+    }
+
+    handleCreate() {
+        let datum = {};
+        this._cols.forEach(col => datum[col.accessor] = '');
+        const data = [...this.state.data, datum];
+        this.setState({data});
+    }
+
+    handleSave() {
+        api.post(api.CATEGORY_API_BULK_UPSERT, this.state.data, (data) => this.setState({data}));
     }
 
     render() {
@@ -48,8 +62,21 @@ class CategoryView extends Component {
             <div>
                 <h1 className="h2">Category Management</h1><hr/>
                 <ReactTable
-                    columns={this.renderColumns()} data={this.state.data}
-                    defaultPageSize={10} className="-striped -highlight"/>
+                    columns={this._cols} data={this.state.data}
+                    defaultPageSize={10} className="-striped -highlight"
+                />
+                <div className="row mt-2">
+                    <div className="col-md-6">
+                        <input type="button" className="btn btn-outline-success mr-2" value="Create" onClick={this.handleCreate}/>
+                        <input type="button" className="btn btn-outline-danger" value="Delete"/>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="float-right">
+                            <input type="button" className="btn btn-outline-primary mr-2" value="Save" onClick={this.handleSave}/>
+                            <input type="button" className="btn btn-outline-warning" value="Reset" onClick={this.handleReset}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
