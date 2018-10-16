7160a6cd47db8848fd0d0f9965351186cfad98d4
Tony Wu <tony.wu90@yahoo.com>
2018-10-15 22:19:32 -0500

    affablebean-admin: attempt JWT example

--- affablebean-admin/src/main/java/tvo/tutorial/microservice/admin/AdminMember.java
+++ affablebean-admin/src/main/java/tvo/tutorial/microservice/admin/AdminMember.java
@@ -1,16 +1,12 @@
 package tvo.tutorial.microservice.admin;
 
-import java.util.List;
-
 public class AdminMember {
     private String username;
     private String password;
-    private List<String> roles;
 
-    public AdminMember(String username, String password, List<String> roles) {
+    public AdminMember(String username, String password) {
         this.username = username;
         this.password = password;
-        this.roles = roles;
     }
 
     public String getUsername() {
@@ -27,13 +23,5 @@ public class AdminMember {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
     }
 }
--- affablebean-admin/src/main/java/tvo/tutorial/microservice/admin/WebSecurityConfig.java
+++ affablebean-admin/src/main/java/tvo/tutorial/microservice/admin/WebSecurityConfig.java
@@ -3,24 +3,36 @@ package tvo.tutorial.microservice.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private CustomUserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public void setUserDetailsService(CustomUserDetailsService userDetailsService) {
+    public void setUserDetailsService(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
     @Autowired
     public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
