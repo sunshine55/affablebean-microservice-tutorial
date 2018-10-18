352273ef5d0157c027c5618f58295d2978921561
Tony Wu <tony.wu90@yahoo.com>
2018-10-18 15:06:10 -0500

    afbb-auth: change properties files to yml format

--- affablebean-proxy/src/main/java/tvo/tutorial/microservice/proxy/ApplicationBoot.java
+++ affablebean-proxy/src/main/java/tvo/tutorial/microservice/proxy/ApplicationBoot.java
@@ -3,6 +3,8 @@ package tvo.tutorial.microservice.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,4 +13,9 @@ public class ApplicationBoot {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationBoot.class, args);
 	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
