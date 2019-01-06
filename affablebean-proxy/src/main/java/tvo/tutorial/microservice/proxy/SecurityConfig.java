db52e7d0b12997b7e581baab06562e2876956b1e
Tony Wu <tony.wu90@yahoo.com>
2019-01-05 20:31:02 -0600

    afbb-auth: apply security filter to gateway

---
+++ affablebean-proxy/src/main/java/tvo/tutorial/microservice/proxy/SecurityConfig.java
@@ -0,0 +1,36 @@
+package tvo.tutorial.microservice.proxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private JwtTokenAuthFilter filter;
+
+    @Autowired
+    public void setFilter(JwtTokenAuthFilter filter) {
+        this.filter = filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf().disable()
+            .logout().disable()
+            .formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .anonymous().and()
+            .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+            .addFilterAfter(filter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests()
+            .antMatchers("/admin/**").permitAll()
+            .antMatchers("/ui/**").permitAll()
+            .anyRequest().authenticated();
+    }
+}
