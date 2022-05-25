c505f3d339e148bc8f2cc061f7ac378b790866e3
sunshine55 <tony.wu90@yahoo.com>
2022-05-24 20:37:27 -0500

    update latest image to reduce volume usage

--- afbb-api/src/main/java/com/sunshine55/tutorial/afbb/api/model/Category.java
+++ afbb-api/src/main/java/com/sunshine55/tutorial/afbb/api/model/Category.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Document(collection = "category")
-@Getter @Setter
+@Data
 public class Category {
     @Id private String id;
     
