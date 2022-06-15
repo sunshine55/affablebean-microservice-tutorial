a789c06af6c0d22bb4ec66cb109ee03d8982bae9
sunshine55 <tony.wu90@yahoo.com>
2022-06-14 20:03:48 -0500

    gui: integrate api initial commit

--- afbb-gui/store-pwa/src/App.js
+++ afbb-gui/store-pwa/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => fetch(`${process.env.REACT_APP_API}/category/getAll`)
+    .then(response => response.json())
+    .then(data => setCategories(data))
+  , []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {categories.map(c => (<li key={c.id}>{c.name}</li>))}
+      </ul>
     </div>
   );
 }
