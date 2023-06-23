ce83444695d3c4135af71aba3e39329d17a5e076
sunshine55 <tony.wu90@yahoo.com>
2023-06-22 20:49:01 -0500

    adjust devContainers configurations and attempt to improve webpack performance

--- afbb-gui-store/webpack.dev.js
+++ afbb-gui-store/webpack.dev.js
@@ -4,10 +4,6 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  watchOptions: {
-    aggregateTimeout: 500,
-    poll: 1000
-  },
   devServer: {
     proxy: [{
       context: ['/category'],
