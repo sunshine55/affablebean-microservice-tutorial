28e95fd54b4738fee2844220a66b00c6f90a2ae1
sunshine55 <tony.wu90@yahoo.com>
2023-06-30 22:49:17 -0500

    afbb-api: add order and customer entities

---
+++ afbb-api/src/main/java/com/sunshine55/tutorial/afbb/api/model/Customer.java
@@ -0,0 +1,21 @@
+package com.sunshine55.tutorial.afbb.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "customer")
+@Getter @Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Customer {
+    @Id private String id;
+
+    private String fullName;
+    private String phone;
+    private String address;
+}
