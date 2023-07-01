a812f81d6ab11b4e2fa01f8b7b2e0278f38e7918
sunshine55 <tony.wu90@yahoo.com>
2023-06-30 22:48:59 -0500

    afbb-api: upgrade spring boot; redesign enitities

--- afbb-api/src/main/java/com/sunshine55/tutorial/afbb/api/model/Item.java
+++ afbb-api/src/main/java/com/sunshine55/tutorial/afbb/api/model/Item.java
@@ -1,3 +1,23 @@
 package com.sunshine55.tutorial.afbb.api.model;
 
-record Item(String name, Float price, String description){}
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "item")
+@Getter @Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Item {
+    @Id private String id;
+
+    private String name;
+    private String description;
+    private Float price;
+
+    @EqualsAndHashCode.Exclude private Category category;
+}
